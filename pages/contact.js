029da17612dc314149d3042420d7f548a7bf7261
aksh-git <ann0m@protonmail.com>
2022-07-15 21:07:44 +0530

    page skeleton

--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import Head from 'next/head'
 
 function contact() {
   return (
-    <section className=''>contact</section>
+    <>
+    <Head>
+      <title>Contact - Akash Yadav | Portfolio</title>
+      <meta name="description" content="Hey there!! i'm Akash Yadav, A passionate fullstack developer from India. check out my profile here.." />
+      <meta name="keywords" content="portfolio, full, stack, web, developer, akash yadav"/>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <div className='container'>
+      <div className='hbadge'>
+        <span className='hbh'>Contact me</span>
+        <span className='hbt'></span>
+      </div>
+    </div>
+    </>
   )
 }
 
--- pages/resume.js
+++ pages/resume.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import Head from 'next/head'
 
 function resume() {
   return (
-    <section>resume</section>
+    <>
+    <Head>
+      <title>Resume - Akash Yadav | Portfolio</title>
+      <meta name="description" content="Hey there!! i'm Akash Yadav, A passionate fullstack developer from India. check out my profile here.." />
+      <meta name="keywords" content="portfolio, full, stack, web, developer, akash yadav"/>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <div className='container'>
+      <div className='hbadge'>
+        <span className='hbh'>Resume</span>
+        <span className='hbt'></span>
+      </div>
+    </div>
+    </>
   )
 }
 
